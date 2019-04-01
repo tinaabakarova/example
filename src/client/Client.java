d82a1e703deb2d9fc441ad0f7358d6b5dee0878a
FAbakarova <fatima.abakarova@aplana.com>
2019-04-01 18:25:48 +0300

    sdxf

--- src/client/Client.java
+++ src/client/Client.java
@@ -4,7 +4,12 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
+/**
+ * @author ilnaz-92@yandex.ru
+ * Created on 2019-03-28
+ */
 public class Client {
+
     public static void main(String[] args) {
         int serverPort = 6666;
 
@@ -21,6 +26,7 @@ public class Client {
             System.out.println("We have a connection");
             System.out.println("************************");
 
+
             InputStream is = socket.getInputStream();
             OutputStream os = socket.getOutputStream();
 
@@ -29,47 +35,69 @@ public class Client {
 
             BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-            while (true) {
-
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
+            Thread sendMsg = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String msg = null;
                         try {
-                            Socket socket = new Socket(ipAddress, serverPort);
-                            String serverMsg = null;
+                            msg = keyboard.readLine();
 
-                            InputStream is = socket.getInputStream();
-                            OutputStream os = socket.getOutputStream();
+                            System.out.println("We are sending to server message = " + msg);
+                            dataOutputStream.writeUTF(msg);
+                            dataOutputStream.flush();
 
-                            DataInputStream dataInputStream = new DataInputStream(is);
-                            DataOutputStream dataOutputStream = new DataOutputStream(os);
+                            String responseFromServer = dataInputStream.readUTF();
 
-                            serverMsg = dataInputStream.readUTF();
-                            dataOutputStream.writeUTF(serverMsg);
+                            if (msg.equalsIgnoreCase("EXIT")) {
+                                System.out.println("I want to exit");
+                                break;
+                            }
+
+                            System.out.println("************************");
+                            System.out.println("We received response from server ==============>>>>>>> " + responseFromServer);
+                            System.out.println("************************");
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+            Thread getMsg = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+
+                            System.out.println("I am waiting info from server...");
+                            String line = dataInputStream.readUTF();
+
+                            if (line.equalsIgnoreCase("EXIT")) {
+                                System.out.println("server want to exit");
+                                break;
+                            }
+
+                            System.out.println("server send a message with text = " + line);
+
+                            dataOutputStream.writeUTF("I received your message. Your message = " + line);
                             dataOutputStream.flush();
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-
                     }
-                });
-                thread.start();
-
-                String msg = keyboard.readLine();
-
-                if (msg.equalsIgnoreCase("EXIT")) {
-                    System.out.println("I want to exit");
-                    break;
                 }
+            });
 
-                dataOutputStream.writeUTF(msg);
-                dataOutputStream.flush();
-
-                thread.join();
-            }
+            sendMsg.start();
+            getMsg.start();
+            sendMsg.join();
+            getMsg.join();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -4,7 +4,12 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * @author ilnaz-92@yandex.ru
+ * Created on 2019-03-28
+ */
 public class Server {
+
     public static void main(String[] args) {
         try {
             int port = 6666;
@@ -14,63 +19,75 @@ public class Server {
             System.out.println("I am waiting a client");
             System.out.println("************************");
 
+
             Socket clientSocket = serverSocket.accept();
             System.out.println("************************");
             System.out.println("I got new client");
             System.out.println("************************");
 
-            InputStream is = clientSocket.getInputStream();
             OutputStream os = clientSocket.getOutputStream();
+            InputStream is = clientSocket.getInputStream();
 
-            DataInputStream dataInputStream = new DataInputStream(is);
             DataOutputStream dataOutputStream = new DataOutputStream(os);
+            DataInputStream dataInputStream = new DataInputStream(is);
 
-            String line = null;
-            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-            while (true)
-            {
-                System.out.println("I am waiting info from client...");
-                line = dataInputStream.readUTF();
+            Thread getMsg = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String line = null;
+                    System.out.println("I am waiting info from client...");
+                    try {
+                        while (true) {
+                            line = dataInputStream.readUTF();
 
-                if (line.equalsIgnoreCase("EXIT"))
-                {
-                    System.out.println("Client want to exit");
-                    break;
+
+                            if (line.equalsIgnoreCase("EXIT")) {
+                                System.out.println("Client want to exit");
+                                break;
+                            }
+
+                            System.out.println("Client send a message with text = " + line);
+
+                            dataOutputStream.writeUTF("I received your message. Your message = " + line);
+                            dataOutputStream.flush();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
+            });
 
-                System.out.println("Client send a message with text = " + line);
+            Thread sendMsg = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-                dataOutputStream.writeUTF("I received your message. Your message = " + line);
-                dataOutputStream.flush();
+                            String msg = keyboard.readLine();
+                            if (msg.equalsIgnoreCase("EXIT")) {
+                                System.out.println("server want to exit");
+                                break;
+                            }
 
-
-
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        String msg = null;
-
-                        try {
-                            OutputStream os = clientSocket.getOutputStream();
-                            DataOutputStream dataOutputStream = new DataOutputStream(os);
-
-                            msg = keyboard.readLine();
                             System.out.println("We are sending to client message = " + msg);
                             dataOutputStream.writeUTF(msg);
                             dataOutputStream.flush();
-                        } catch (IOException e) {
-                            e.printStackTrace();
                         }
-
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                });
-                thread.start();
-                thread.join();
-            }
-
+                }
+            });
+            getMsg.start();
+            sendMsg.start();
+            getMsg.join();
+            sendMsg.join();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
 }
